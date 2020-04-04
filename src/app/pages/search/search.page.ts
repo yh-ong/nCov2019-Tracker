8ae5d2b34e8194dc5b564e40646660fa0fdc0fc3
YH ONG <ongyh97@gmail.com>
2020-04-04 21:14:47 +0800

    feat: add filter search

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProviderService } from 'src/app/services/provider.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  countryItems: any;
+
+  constructor(private providerSvc: ProviderService, private router: Router) { }
 
   ngOnInit() {
+    this.getCountry();
+  }
+
+  getCountry() {
+    this.providerSvc.getData(this.providerSvc.API_URL).subscribe(
+      res => {
+      this.countryItems = Object.keys(res);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  onSearch(searchData:any) {
+    const val = searchData.target.value;
+    if (val && val.trim() != '') {
+      this.countryItems = this.countryItems.filter((item) => {
+        var itemname = item;
+        return (itemname.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    } else {
+      this.getCountry();
+    }
   }
 
 }
