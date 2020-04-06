3ca4011f6c2592b399b4b12896fef3698e19fa89
YH ONG <ongyh97@gmail.com>
2020-04-06 15:53:41 +0800

    style: change path direction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { DataService } from './services/data.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'tabs/home', pathMatch: 'full' },
-  /* { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-    path: 'search',
-    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
-  }, */
+  // { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  // { path: 'search', loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)},
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
